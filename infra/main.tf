d7c4f1f3748690543c91bf574c3c5578486842e9
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-04-13 22:16:01 +0200

    Implemented IAM role for ECS task execution

--- infra/main.tf
+++ infra/main.tf
@@ -9,3 +9,21 @@ resource "aws_ecr_repository" "my_ecr_repo"{
       scan_on_push = true
     }
 }
+
+# IAM role for ECS task execution
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecs-task-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
