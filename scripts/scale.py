cdfd41a0656ce888d649d7f4eef5369c8428becd
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-04-20 20:30:19 +0200

    Implemented first iteration of getting metrics from CW

--- scripts/scale.py
+++ scripts/scale.py
@@ -1,6 +1,34 @@
-
+from datetime import datetime, timedelta
+import boto3
 import logging
 import typer
 
 app = typer.Typer(help="Scale ECS tasks based on CW logs for cloud-automation-platform")
 logger = logging.getLogger(__name__)
+
+SECONDS_IN_A_DAY = 86400
+
+cloudwatch = boto3.client("cloudwatch")
+
+response = cloudwatch.get_metric_statistics(
+    Namespace = 'AWS/ECS',
+    Dimensions = [
+        {
+            'Name' : 'app',
+            "Value": "CPUUtilization"
+        },
+        {
+            'Name': 'app',
+            'Value': "MemoryUtilization"
+        }
+    ],
+    MetricName = 'ScaleMetric',
+    StartTime = datetime.now() - timedelta(days=7),
+    EndTime = datetime.now(),
+    Period = SECONDS_IN_A_DAY,
+    Statistics = [
+        'Average'
+    ],
+    Unit = 'Bytes'
+)
+
