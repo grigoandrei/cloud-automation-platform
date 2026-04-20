2abef19b5a2b2efa42cf22c6c4818fe0353d9dc8
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-04-20 19:55:40 +0200

    Implemented ECS service

--- infra/main.tf
+++ infra/main.tf
@@ -107,3 +107,16 @@ resource "aws_cloudwatch_log_group" "app" {
   name              = "/ecs/cloud-automation-platform"
   retention_in_days = 7
 }
+
+resource "aws_ecs_service" "app" {
+  name = "cloud-automation-service"
+  cluster = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets = aws_subnet.private_subnets[*].id
+    security_groups = [aws_security_group.ecs_security_group.id]
+  }
+}
