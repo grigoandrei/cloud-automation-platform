2981145ef753eaa18702b8c8b5301235b8bc8d14
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-04-21 21:29:11 +0200

    Created ECS task definition

--- infra/main.tf
+++ infra/main.tf
@@ -120,3 +120,30 @@ resource "aws_ecs_service" "app" {
     security_groups = [aws_security_group.ecs_security_group.id]
   }
 }
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "cloud-automation-platform"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([{
+    name      = "app"
+    image     = "${aws_ecr_repository.my_ecr_repo.repository_url}:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      protocol      = "tcp"
+    }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        "awslogs-group"         = aws_cloudwatch_log_group.app.name
+        "awslogs-region"        = "eusc-de-east-1"
+        "awslogs-stream-prefix" = "ecs"
+      }
+    }
+  }])
+}
