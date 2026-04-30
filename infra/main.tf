dee8d29d94e8cca682db57f895b03ef018564979
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-04-30 21:52:04 +0200

    Added VPC endpoints for ECS,S3 and CW

--- infra/main.tf
+++ infra/main.tf
@@ -80,6 +80,40 @@ resource "aws_security_group" "ecs_security_group" {
   
 }
 
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id = aws_vpc.main.id
+  service_name = "com.amazonaws.eusc-de-east-1.ecr.api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = aws_subnet.private_subnets[*].id
+  security_group_ids = [aws_security_group.vpc_endpoints_security_group.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.eusc-de-east-1.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private_subnets[*].id
+  security_group_ids  = [aws_security_group.vpc_endpoints_security_group.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.eusc-de-east-1.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.eusc-de-east-1.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private_subnets[*].id
+  security_group_ids  = [aws_security_group.vpc_endpoints_security_group.id]
+  private_dns_enabled = true
+}
+
 resource "aws_security_group" "vpc_endpoints_security_group" {
   name = "vpc-endpoints-sg"
   description = "Security group for VPC Endpoints"
